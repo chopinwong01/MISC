bc5251b234d85bd47994aece0fbf98020f96f001
Chopin <chopinwong01@gmail.com>
2015-04-08 23:01:39 +1000

    should work fine without warning

--- countArray.c
+++ countArray.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int *countArray(char *a){
+int *countArray(char* filename){
+  FILE *fp;
+  char *a;
+  int c=0;
+  fp = fopen(filename,"r");
+  fseek(fp,0,SEEK_END);
+  int pos = ftell(fp);
+  fclose(fp);
+  fp = fopen(filename,"r");
+  a=(char*)calloc(pos,sizeof(int));
+  while(1)
+  {
+    a[c] = fgetc(fp);
+    if(feof(fp)){break;}
+    c++;
+  }
+  fclose(fp);
+
   int *r;
   r= (int*)calloc(26, sizeof(int));
-
   for (int i = 0; i < 26; ++i)
   { 
     r[i]=0;
   }
   int i = 0;
-  while( a[i] != NULL){
+  while( a[i] != (char)NULL){
     int j = a[i] - 65;
     if( a[i] < 123 && a[i] > 96){j = a[i] - 97;}
     if( j < 26 && j >= 0){
@@ -21,11 +38,9 @@ int *countArray(char *a){
   return r;
 }
 
-int main(){
-  char *a;
-  gets(a);
+int main(int argc, char* argv[]){
   int *r;
-  r=countArray(a);
+  r=countArray(argv[1]);
   printf("(%d characters)\n",r[26]);
 
   printf("characters\t|\t");
@@ -57,9 +72,5 @@ int main(){
     printf("%d\t",r[i]);
   }
   printf("\n");
-
-
-
-
   return 0;
 }
